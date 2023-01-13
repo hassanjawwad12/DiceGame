263edd516731a8b8fb45ee240081d826b6e2e747
Muhammad Hassan Jawwad <113373773+hassanjawwad12@users.noreply.github.com>
2023-01-13 21:19:39 +0500

    Add files via upload

---
+++ DOM/index.js
@@ -0,0 +1,30 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage= "Dice" + randomNumber1 + ".png";
+
+var randomImageSource= "images/" + randomDiceImage;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomImageSource2= "images/Dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+//if player 1 wins
+if(randomNumber1>randomNumber2)
+{
+  document.querySelector("h1").innerHTML = "🥇Player 1 wins";
+}
+else if(randomNumber2>randomNumber1)
+{
+  document.querySelector("h1").innerHTML = "🥇Player 2 wins";
+}
+else
+{
+  document.querySelector("h1").innerHTML = "Draw";
+}
